c6691a5e4279e4a3e0283e7a5c848c3ea96292da
Sol <ccw0711@naver.com>
2020-05-28 15:42:51 +0900

    코드 정리 롤백

--- main.py
+++ main.py
@@ -6,41 +6,53 @@ import data
 import random
 import time
 
-base = os.path.dirname(os.path.abspath(__file__)) + '/'
-with open(base + 'acc.txt') as f:
+base = os.path.dirname(os.path.abspath(__file__))+'/'
+with open(base+'acc.txt') as f:
     acc = f.read().strip()
-headers = {'Authorization':'Bearer ' + acc}
+headers = {'Authorization':'Bearer '+acc}
 instance = 'https://twingyeo.kr'
 
-def toot(message, toot_id):
-    global toot_type
+def toot(message, toot_id, toot_type):
     d = dict()
     d['status'] = message
     d['visibility'] = toot_type
-    d['in_reply'] = toot_id
-    requests.post(instance + '/api/v1/statuses', headers = headers, data = d)
+    d['in_reply_to_id'] = toot_id
+    requests.post(instance+'/api/v1/statuses', headers=headers, data=d)
 
 def follow(n):
-    requests.post(instance + '/api/v1/accounts/' + str(n) + '/follow', headers = headers)
+    requests.post(instance+'/api/v1/accounts/'+str(n)+'/follow', headers=headers)
 
-uri = instance + '/api/v1/streaming/user'
-timeline = requests.get(uri, headers = headers, stream = True)
+uri = instance+'/api/v1/streaming/user'
+timeline = requests.get(uri, headers=headers, stream=True)
 for t in timeline.iter_lines():
     dec = t.decode('utf-8')
-    if dec == 'event: notification':
-        newdec = json.loads(re.sub('data: ', '', dec))
-        if newdec['type'] == 'mention':
-            if newdec['account']['bot'] == False:
-                msg = random.choice(data.kotoba)
-                toot_id = newdec['status']['id']
-                reply_to_account = newdec['account']['acct']
-                message = '@' + reply_to_account + ' ' + msg
-                toot_type = newdec['status']['visibility']
-                if toot_type == 'public':
-                    toot_type = 'unlisted'
-                toot(message, toot_id)
-            else:
-                pass
-        elif newdec['type'] == 'follow':
-            n = newdec['account']['id']
-            follow(n)
+    if dec == 'event: update':
+        stat = 1
+    elif dec == ':)':
+        stat = 1
+    elif dec == 'event: notification':
+        stat = 0
+    elif dec == ':thump':
+        stat = 1
+    if stat:
+        pass
+    else:
+        try:
+            newdec = json.loads(re.sub('data: ','',dec))
+            if newdec['type'] == 'mention':
+                if newdec['account']['bot'] == False:
+                    msg = random.choice(data.kotoba)
+                    toot_id = newdec['status']['id']
+                    reply_to_account = newdec['account']['acct']
+                    message = '@'+reply_to_account+' '+msg
+                    toot_type = newdec['status']['visibility']
+                    if toot_type == 'public':
+                        toot_type = 'unlisted'
+                    toot(message, toot_id, toot_type)
+                else:
+                    pass
+            elif newdec['type'] == 'follow':
+                n = newdec['account']['id']
+                follow(n)
+        except:
+            pass
